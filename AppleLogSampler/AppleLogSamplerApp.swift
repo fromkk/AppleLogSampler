e0cfd0497cf2e3a981cd441bf353e4c30c8f597e
Kazuya Ueoka <info@fromkk.info>
2024-03-28 16:59:06 +0900

    Initial Commit

---
+++ AppleLogSampler/AppleLogSamplerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleLogSamplerApp.swift
+//  AppleLogSampler
+//
+//  Created by Kazuya Ueoka on 2024/03/28.
+//
+
+import SwiftUI
+
+@main
+struct AppleLogSamplerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
